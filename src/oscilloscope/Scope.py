0298b59fb60f51c27df50645b9cc35ce8acb03d1
Kate Endersby <kateende@iastate.edu>
2025-12-05 16:14:35 -0600

    non functioning

--- src/oscilloscope/Scope.py
+++ src/oscilloscope/Scope.py
@@ -75,6 +75,7 @@ class Scope:
         return t_axis
     
     def get_ecg_time_axis(self, samples):
+        print(samples)
         return np.linspace(0, len(samples) / self.ecg_sample_rate, len(samples))
 
     def get_reaction_time_axis(self, samples):
